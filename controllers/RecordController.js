59b088157247efcf0192ea0644b6f7a91f344cd6
Ahmet Kavuğubüyük <pgahmet@gmail.com>
2021-04-03 19:55:46 +0300

    error handling added to database connection

--- controllers/RecordController.js
+++ controllers/RecordController.js
@@ -13,55 +13,58 @@ const { validationResult } = require("express-validator");
  *
  * @returns {Object}
  */
-exports.filter = [
-  (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return apiResponse.errorResponse(
-        res,
-        422,
-        "Validation Error",
-        errors.array()
-      );
-    }
-    try {
-      const { startDate, endDate, minCount, maxCount } = req.body;
-      getRecords(startDate, endDate, minCount, maxCount).then((items) => {
-        if (items.length > 0) {
-          apiResponse.customResponse(res, 200, "Success", items);
-        } else {
-          apiResponse.errorResponse(res, 404, "Record not found!");
-        }
-      });
-    } catch (error) {
-      apiResponse.errorResponse(
-        res,
-        500,
-        "Internal server error!",
-        error.message
-      );
-    }
-  },
-];
+exports.filter = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return apiResponse.errorResponse(
+      res,
+      422,
+      "Validation Error",
+      errors.array()
+    );
+  }
+  try {
+    const { startDate, endDate, minCount, maxCount } = req.body;
+    await getRecords(startDate, endDate, minCount, maxCount).then((items) => {
+      if (items && items.length > 0) {
+        apiResponse.customResponse(res, 200, "Success", items);
+      } else {
+        apiResponse.errorResponse(res, 404, "Record not found!");
+      }
+    });
+  } catch (error) {
+    apiResponse.errorResponse(
+      res,
+      500,
+      "Internal server error!",
+      error.message
+    );
+  }
+};
 
 const getRecords = async (startDate, endDate, minCount, maxCount) => {
-  return await Record.aggregate([
-    {
-      $project: {
-        _id: false,
-        key: true,
-        createdAt: true,
-        totalCount: { $sum: "$counts" },
-      },
-    },
-    {
-      $match: {
-        totalCount: { $gte: parseInt(minCount), $lte: parseInt(maxCount) },
-        createdAt: {
-          $gte: new Date(startDate),
-          $lte: new Date(endDate),
+  try {
+    const records = await Record.aggregate([
+      {
+        $project: {
+          _id: false,
+          key: true,
+          createdAt: true,
+          totalCount: { $sum: "$counts" },
         },
       },
-    },
-  ]);
+      {
+        $match: {
+          totalCount: { $gte: parseInt(minCount), $lte: parseInt(maxCount) },
+          createdAt: {
+            $gte: new Date(startDate),
+            $lte: new Date(endDate),
+          },
+        },
+      },
+    ]);
+    return records;
+  } catch (error) {
+    throw new Error("Database connection error!");
+  }
 };
